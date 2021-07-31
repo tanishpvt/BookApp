b00b956cf04c714f49555d80ef0cebd9d18c6274
Tanish Mohanty <tanish0911@gmail.com>
2021-07-31 20:10:16 +0530

    Initial commit

---
+++ app/src/main/java/com/example/bookapp/SplashScreen/SplashScreen.java
@@ -0,0 +1,31 @@
+package com.example.bookapp.SplashScreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+import com.example.bookapp.MainActivity;
+import com.example.bookapp.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen2);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },4000);
+
+    }
+}
